91130f668ef72b28768fc75e7c33f97b1f073830
MarquesDev <marques.developpement@gmail.com>
2019-03-02 13:54:56 +0100

    move

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import App from "./components/App/App";
 import * as serviceWorker from "./serviceWorker";
 import { initSentry } from "./utils/sentry";
 import { Amplitude } from "./utils/amplitude";
