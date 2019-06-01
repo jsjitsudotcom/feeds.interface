7cff1f6028fdf9115db295a3dd697d1a9ea7ce53
MarquesDev <marques.developpement@gmail.com>
2019-06-01 22:02:34 +0200

    temporary circleci tutorial

--- src/index.js
+++ src/index.js
@@ -3,19 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App/App";
 import * as serviceWorker from "./serviceWorker";
-import { initSentry } from "./utils/sentry";
-import { Amplitude } from "./utils/amplitude";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-const isConnectedInPWA = window.location.search.indexOf("source=pwa") > -1;
-
-Amplitude.logEvent("CONNECT", {
-  pwa: isConnectedInPWA,
-  referrer: document.referrer
-});
-
-initSentry();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
--- src/utils/sentry.js
+++
@@ -1,9 +0,0 @@
-import * as Sentry from "@sentry/browser";
-
-export const initSentry = () => {
-  Sentry.init({
-    dsn: process.env.SENTRY_DSN,
-    release: process.env.VERSION,
-    environment: process.env.NODE_ENV
-  });
-};
